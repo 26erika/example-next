8b7f0a9197521e2fe4b8ceb3ec72039e4439bad7
Erika Sanchez <lilixxxzaz@gmail.com>
2019-09-16 09:28:10 -0500

    Seagrega parrafo a pagina de about

--- pages/about.js
+++ pages/about.js
@@ -10,7 +10,10 @@ const About = () => (
       <Layout>
         <Content>
           <Header>About</Header>
-          <Card><h1>This is about page</h1></Card>
+          <Card>
+            <h1>This is about page from about page</h1>
+            <p>Hello World</p>
+          </Card>
         </Content>
       </Layout>
     
