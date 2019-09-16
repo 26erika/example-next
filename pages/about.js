afc730df0cf8a4280dc62c1422b59deab4b2b130
Erika Sanchez <lilixxxzaz@gmail.com>
2019-09-16 08:51:41 -0500

    Se agrega seccion de about

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import Layout from '../components/Layout';
+import Content from '../components/Content';
+import Header from '../components/Header';
+import Card from '../components/Card';
+
+
+
+const About = () => (
+    
+      <Layout>
+        <Content>
+          <Header>About</Header>
+          <Card><h1>This is about page</h1></Card>
+        </Content>
+      </Layout>
+    
+)
+
+export default About ;
+    
